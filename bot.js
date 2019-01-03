3abd8fe07d79e05047109b1f02df802f1d9b44f7
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2019-01-03 07:47:37 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
-const prefix = "r#";
+const prefix = "d#";
 // Toxic Codes
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
-          client.user.setActivity("Rainbow , r#help .",{type: 'WATCHING'});
+          client.user.setActivity("Dream Server",{type: 'WATCHING'});
   
   });// Toxic Codes
 client.on("message", message => {
@@ -37,44 +37,25 @@ client.on("message", message => {
     switch(command) {
         case "set" :
         if(!message.member.hasPermission('ADMINSTRATOR')) return message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINSTRATOR`' );
-        message.guild.createRole({name : "RainbowBot .", color : "RANDOM"}).then(r => {// Toxic Codes
+        message.guild.createRole({name : "drr.", color : "RANDOM"}).then(r => {// Toxic Codes
             r.edit({color : "RANDOM"});
             suck[message.guild.id] = {role : r.id};// Toxic Codes
         });// Toxic Codes
     };// Toxic Codes
 });// Toxic Codes
 client.on("message", message => {
-  if (message.content === "r#help") {
+  if (message.content === "d#help") {
       message.react('🌈')
 message.author.send(`**
-r#set 
- - لإنشاء رتبة الرينبو وبدا الرينبو
-- To create the role of the Rainbow & Start The Rainbow
-r#inv 
-- لدعوة البوت
-- To Invite the bot
+d#set 
+ -ﻹنشاء الرتبة
+- To create the role of the  dream & Start The drr
 // Toxic Codes// Toxic Codes// Toxic Codes
 خطوات لو الرتبة م أشتغلت .!!
-1- ضع رتبة الرينبو فوق الالوان أو الرتب الملونه لو فيه// Toxic Codes
-2- ضع رتبة البوت فوق رتبة الرينبو
-The steps of the role did not worked .!!// Toxic Codes
-1- Place the role of the Rainbow above the colors or colored ranks if it
-2- Put the bot role above the role of the Rainbow // Toxic Codes
-رآبط البوت - Bot Invite link// Toxic Codes
-- http://cutt.us/RainbowBott
+1- ضع رتبة فوق الالوان أو الرتب الملونه لو فيه// Toxic Codes
+2- ضع رتبة البوت فوق رتبة 
+
 **`)// Toxic Codes
-  }})
-   client.on('message', message => {
-	   if(message.content.startsWith(`r#inv`)){
-		   if(!message.channel.guild) return message.channel.send("This Command is Just For Servers!")
-                 message.react('🌈')// Toxic Codes// Toxic Codes
-		   var embed = new Discord.RichEmbed()
-		   .setTitle(">> ClickHere To Add" + `${client.user.username}` + " <<")// Toxic Codes// Toxic Codes// Toxic Codes
-		   .setURL("https://discordapp.com/oauth2/authorize?client_id=" + `${client.user.id}` + "&scope=bot&permissions=2080374975")// Toxic Codes
-		   .setTimestamp()// Toxic Codes// Toxic Codes// Toxic Codes// Toxic Codes
-		   .setFooter(`Requested By | ${message.author.username}`)// Toxic Codes
-		   .setColor("RANDOM")
-		   message.author.send({embed})// Toxic Codes// Toxic Codes// Toxic Codes
-	   }// Toxic Codes// Toxic Codes// Toxic Codes// Toxic Codes// Toxic Codes
-   });
+  });
+   
 client.login(process.env.BOT_TOKEN)
